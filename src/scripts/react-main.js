9919ebac378ec5cd2be99dff819172177fcad1dd
Rasika Weliwita <rasika.weliwita@cslworld.com>
2016-11-14 14:15:25 +0530

    integrated react

---
+++ src/scripts/react-main.js
@@ -0,0 +1,6 @@
+$ = jQuery = require('jquery');
+var React = require('react');
+var ReactDOM = require('react-dom');
+var Home = require('./components/homePage');
+
+ReactDOM.render(<Home/>, document.getElementById('app'));
